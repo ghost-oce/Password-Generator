2a71a3e5a104f3c99620e1caa11bccc437b3d4f0
Andrew <andrewjmeng@hotmail.com>
2021-12-11 13:23:35 +1100

    adding original code incase of any major changes

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+//original code: var generateBtn = document.querySelector("#generate");
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+  // passwordText.value = password;
+  //}
+  //generateBtn.addEventListener("click", writePassword);
+
 //Prompts needed to be shown:
 //1. the user needs a length of at least 8 characters and no more than 128 characters
 //2. the user needs lower case
-//3. the user needs upper case
+//3. the user needs upper case 
 //4. the user needs numbers
-//5. the user needs special characters
+//5. the user needs special characters - special characters include: !@#$%^&*()''"
 
 //First step would be create vars for length, lower case, upper case, numbers and special characters?
 
